da74aead2b478a6066e031c4ee8f172e2785f764
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-04 11:52:10 +0600

    feat: integrate header and footer into layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       {/* Featured Section */}
-      <section className="w-11/12 lg:container mx-auto mt-24">
+      <section className="w-11/12 lg:container mx-auto mt-24 min-h-screen">
         <FeaturedCategories />
       </section>
     </main>
