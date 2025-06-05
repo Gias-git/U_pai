9d05db00113cf411059742ae9af74995b79bba91
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-05 22:25:23 +0600

    feat: remove Featured section from the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import FeaturedCategories from "@/components/FeaturedCategories";
 import OurProducts from "@/components/OurProducts";
-
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* Featured Section */}
-      <section className="w-11/12 lg:container mx-auto mt-24">
-        <FeaturedCategories />
-      </section>
-
       {/* Our Product Section */}
       <section className="w-11/12 lg:container mx-auto my-24">
         <OurProducts />
