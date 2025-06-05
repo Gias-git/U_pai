71a41647f7e29b501924da5530bdb6186e1d2c15
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-05 13:16:06 +0600

    feat: add <OurProducts /> section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import FeaturedCategories from "@/components/FeaturedCategories";
+import OurProducts from "@/components/OurProducts";
 
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen">
       {/* Featured Section */}
-      <section className="w-11/12 lg:container mx-auto mt-24 min-h-screen">
+      <section className="w-11/12 lg:container mx-auto mt-24">
         <FeaturedCategories />
+      </section>
+
+      {/* Our Product Section */}
+      <section className="w-11/12 lg:container mx-auto my-24">
+        <OurProducts />
       </section>
     </main>
   );
