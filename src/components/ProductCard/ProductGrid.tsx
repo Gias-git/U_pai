933d6a2d426c9b01aa08d78ae4de4a890d4aed4d
Gias-git <giasuddin.programmer@gmail.com>
2025-06-16 21:04:41 +0600

    card-mobile-resposive

--- src/components/ProductCard/ProductGrid.tsx
+++ src/components/ProductCard/ProductGrid.tsx
@@ -7,11 +7,12 @@ interface ProductGridProps {
 
 const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
       {products.map((product, idx) => (
         <ProductCard key={idx} {...product} />
       ))}
     </div>
+
   );
 };
 
